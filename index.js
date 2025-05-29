fc2b7ba7836ad567d598825d9488d64307a5853a
IVENS DIEGO MULLER <ivens.muller@prof.sc.senac.br>
2025-05-28 21:32:15 -0300

    adicionando lista de endereços

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const listaStorage = localStorage.getItem("listaEnderecos")
+let listaEnderecos = [];
+if (listaStorage) {
+  listaEnderecos = JSON.parse(listaStorage);
+}
+
 function buscarCep() {
   const inputCep = document.getElementById("input_cep");
   const valorCep = inputCep.value;
@@ -7,16 +13,57 @@ function buscarCep() {
       return resposta.json();
     })
     .then((json) => {
-      console.log("O estado é " + json.state);
-      console.log("A cidade é " + json.city);
-      console.log("O bairro é " + json.neighborhood);
-      console.log("A rua é " + json.street);
+      console.log(json);
+      const inputState = document.getElementById("input_state");
+      inputState.value = json.state;
+
+      const inputCity = document.getElementById("input_city");
+      inputCity.value = json.city;
+
+      const inputNeighborhood = document.getElementById("input_neighborhood");
+      inputNeighborhood.value = json.neighborhood;
+
+      const inputStreet = document.getElementById("input_street");
+      inputStreet.value = json.street;
     });
+}
+
+function clicarSalvar() {
+  console.log("Clicou para salvar");
+  const inputCep = document.getElementById("input_cep");
+  const valorCep = inputCep.value;
+
+  const inputState = document.getElementById("input_state");
+  const valorState = inputState.value;
+
+  const inputCity = document.getElementById("input_city");
+  const valorCity = inputCity.value;
+
+  const inputNeighborhood = document.getElementById("input_neighborhood");
+  const valorNeighborhood = inputNeighborhood.value;
+
+  const inputStreet = document.getElementById("input_street");
+  const valorStreet = inputStreet.value;
+
+  const novoEndereco = {
+    cep: valorCep,
+    state: valorState,
+    city: valorCity,
+    neighborhood: valorNeighborhood,
+    street: valorStreet
+  }
+
+  listaEnderecos.push(novoEndereco);
+  console.log(listaEnderecos);
+  localStorage.setItem("listaEnderecos", JSON.stringify(listaEnderecos))
 }
 
 function configurarEventos() {
   const inputCep = document.getElementById("input_cep");
   inputCep.addEventListener("focusout", buscarCep);
+
+  const botaoSalvar = document.getElementById("botao_salvar");
+  botaoSalvar.addEventListener("click", clicarSalvar)
 }
 
 window.addEventListener("load", configurarEventos);
